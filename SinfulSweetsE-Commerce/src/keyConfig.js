790aa66a223c01b3221178e0fe8256c8ea339efa
Nicholas Caldwell <86279327+CaldoNic7@users.noreply.github.com>
2021-12-26 09:39:13 -0500

    added project server keys and updated keyConfig to swtich between them dynamically

--- SinfulSweetsE-Commerce/src/keyConfig.js
+++ SinfulSweetsE-Commerce/src/keyConfig.js
@@ -1,18 +1,34 @@
+// keyConfig chooses between three sets of keys depending on the hostname (local host or the live website) and a variable i change manually in order to switch between the live site server and my project server. This allows me to use this site as a project without interfering with  my clients business. 
+
 let KEYS
+let project = true
+
+// Jessica's commercejs.com and stripe.com accounts sandbox keys for development. 
 const devKeys = {
   chec: process.env.REACT_APP_CHEC_SANDBOX,
   stripe: process.env.REACT_APP_STRIPE_SANDBOX
 }
 
+// Jessica's commercejs.com ans stripe.com accounts live keys.
 const liveKeys = {
 	chec: process.env.REACT_APP_CHEC_PUBLIC_KEY,
 	stripe: process.env.REACT_APP_STRIPE_PUBLIC_KEY,
 }
 
-if (window.location.hostname === 'localhost') {
-  KEYS = devKeys
+// My commercejs.com and stripe.com accounts sandbox keys for use as a project on my portfolio. 
+const projDevKeys = {
+	chec: process.env.REACT_APP_CHEC_SANDBOX_PROJ,
+	stripe: process.env.REACT_APP_STRIPE_SANDBOX_PROJ,
+}
+
+if (window.location.hostname === 'localhost' && project === false) {
+  KEYS=devKeys
+} else if (project === true){
+  KEYS=projDevKeys
 } else {
-  KEYS = liveKeys
+  KEYS=liveKeys
 }
 
 export default KEYS
+
+console.log('keys', KEYS)
